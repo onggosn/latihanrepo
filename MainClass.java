29c17f4f3fc8c04dca56098bf8059603004703d2
Onggo <onggo.nugroho@gmail.com>
2019-11-05 09:59:44 +0700

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -2,6 +2,6 @@ import java.io.*;
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Versi 1");
-		System.out.println("Hello Pemula GITT!!!!!!");
+		System.out.println("Hello Pemula git!!!!!!");
 	}
 }
